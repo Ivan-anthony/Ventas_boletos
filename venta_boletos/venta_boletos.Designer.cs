d86ae53d26986e24bef1bd70303fb4c9003ac987
Ivan-anthony <158600608+Ivan-anthony@users.noreply.github.com>
2024-04-17 11:14:09 -0500

    Add files via upload

---
+++ venta_boletos/venta_boletos.Designer.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Drawing;
+
+namespace ventaslibreria
+{
+    partial class TravelRoutesForm
+    {
+        private const string nombre = "nombre:";
+
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("boleto");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("numero ");
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("nombre");
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem("fecha");
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.boleto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = nombre;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ruta";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "precio";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(175, 70);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(132, 22);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(175, 145);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(132, 22);
+            this.textBox6.TabIndex = 5;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Sacsayhuaman",
+            "Puka Pukara",
+            "Tambomachay",
+            "Tipon-Lucre-Piquillaqta",
+            "Ollantaytambo",
+            "Machupicchu"});
+            this.comboBox3.Location = new System.Drawing.Point(163, 221);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(163, 297);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(121, 22);
+            this.textBox7.TabIndex = 8;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.boleto});
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4});
+            this.listView1.Location = new System.Drawing.Point(410, 51);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(463, 352);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // TravelRoutesForm
+            // 
+            this.ClientSize = new System.Drawing.Size(957, 463);
+            NewMethod();
+            object value = GetValue();
+            object value1 = this.Controls.Add(this.comboBox3);
+            object value2 = this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TravelRoutesForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            void NewMethod()
+            {
+                object value = this.Controls.Add(this.listView1);
+            }
+        }
+
+        private object GetValue()
+        {
+            return this.Controls.Add(this.textBox7);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SuspendLayout()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader boleto;
+
+        public Size ClientSize { get; private set; }
+        public object Controls { get; private set; }
+    }
+}
+
---
+++ venta_boletos/venta_boletos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public partial class TravelRoutesForm : Form
+{
+    private List<TravelRoute> travelRoutes;
+
+    public TravelRoutesForm()
+    {
+        InitializeComponent();
+
+        // Load travel route data (replace with your actual data loading logic)
+        travelRoutes = new List<TravelRoute>()
+        {
+            new TravelRoute() { RouteId = 1, Origin = "Lima", Destination = "Cusco", TravelTime = 6 },
+            new TravelRoute() { RouteId = 2, Origin = "Arequipa", Destination = "Puno", TravelTime = 4 },
+            new TravelRoute() { RouteId = 3, Origin = "Trujillo", Destination "Chiclayo", TravelTime = 3 },
+        };
+
+        // Bind travel routes to list view
+        listViewRoutes.Items.Clear();
+        foreach (TravelRoute route in travelRoutes)
+        {
+            ListViewItem item = new ListViewItem(route.RouteId.ToString());
+            item.SubItems.Add(route.Origin);
+            item.SubItems.Add(route.Destination);
+            item.SubItems.Add(route.TravelTime.ToString() + " horas");
+            listViewRoutes.Items.Add(item);
+        }
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label3_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
+
+public class TravelRoute
+{
+    public int RouteId { get; set; }
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+    public int TravelTime { get; set; } // In hours
+}
